cfb71824a2a63387a8c28309c670feb37d879047
Pawan Kumar <pawan2017cht@gmail.com>
2025-02-23 22:03:33 +0530

    Creating table admin and DeleveryAgent

--- models.py
+++ models.py
@@ -18,3 +18,40 @@ class Customer(UserMixin, db.Model):
     # this method is required for Flask-Login
     def get_id(self):
         return (self.id)
+    
+class Admin(UserMixin, db.Model):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+
+
+    def __repr__(self):
+        return f'<Admin {self.username}>'
+    
+    # this method is required for Flask-Login
+    def get_id(self):
+        return (self.id)
+    
+class DeleveryAgent(UserMixin, db.Model):
+    __tablename__ = 'delevery_agent'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    phone = db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    delevery_area = db.Column(db.String(100), nullable=False)
+    # available_slots = db.Column(db.Boolean, nullable=False)
+    available_slots = db.Column(db.Boolean, nullable=False, default=True)
+
+
+    
+    def __repr__(self):
+        return f'<DeleveryAgent {self.username}>'
+    
+    # this method is required for Flask-Login
+    def get_id(self):
+        return (self.id)
+    

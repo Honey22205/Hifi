5380f891df995ccff9e5ed0c2b07061fe9adcd4e
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-02-26 14:55:11 +0530

    Add admin dashboard and delivery agent management features; include new templates, styles, and database migrations

--- models.py
+++ models.py
@@ -1,6 +1,4 @@
-# Creating sample database for testing
-
-from flask_login import UserMixin # include this in model because it help to manage login state
+from flask_login import UserMixin
 from app import db
 
 class Customer(UserMixin, db.Model):
@@ -8,50 +6,43 @@ class Customer(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
-    phone = db.Column(db.Integer, unique=True, nullable=False) 
+    phone = db.Column(db.Integer,unique=True, nullable=False) 
     password = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(100), nullable=False)
     
     def __repr__(self):
         return f'<Customer {self.username}>'
     
-    # this method is required for Flask-Login
+    # Flask-Login required method
     def get_id(self):
-        return (self.id)
-    
+        return self.id
+
 class Admin(UserMixin, db.Model):
     __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)  
     phone = db.Column(db.Integer, unique=True, nullable=False) 
-
 
     def __repr__(self):
         return f'<Admin {self.username}>'
     
-    # this method is required for Flask-Login
     def get_id(self):
-        return (self.id)
-    
-class DeleveryAgent(UserMixin, db.Model):
-    __tablename__ = 'delevery_agent'
+        return self.id
+
+class DeliveryAgent(UserMixin, db.Model):
+    __tablename__ = 'delivery_agent'  
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     phone = db.Column(db.Integer, unique=True, nullable=False) 
     password = db.Column(db.String(100), nullable=False)
-    delevery_area = db.Column(db.String(100), nullable=False)
-    # available_slots = db.Column(db.Boolean, nullable=False)
+    delivery_area = db.Column(db.String(100), nullable=False)  
     available_slots = db.Column(db.Boolean, nullable=False, default=True)
-
-
     
     def __repr__(self):
-        return f'<DeleveryAgent {self.username}>'
+        return f'<DeliveryAgent {self.username}>'
     
-    # this method is required for Flask-Login
     def get_id(self):
-        return (self.id)
-    
+        return self.id
